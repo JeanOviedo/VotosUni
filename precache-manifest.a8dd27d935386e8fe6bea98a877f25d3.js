e5c2d2f83d9ffef8a84c90e0b506078f4a0dbdb3
JeanOviedo <85713073+JeanOviedo@users.noreply.github.com>
2022-07-15 17:15:46 -0500

    Updates

---
+++ precache-manifest.a8dd27d935386e8fe6bea98a877f25d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69747568e7699e24740e",
+    "url": "/VotosUni/static/css/main.d02ee504.chunk.css"
+  },
+  {
+    "revision": "69747568e7699e24740e",
+    "url": "/VotosUni/static/js/main.374b7669.chunk.js"
+  },
+  {
+    "revision": "7fe5cc8f4b221c9fe5a9",
+    "url": "/VotosUni/static/js/runtime~main.538cf1d1.js"
+  },
+  {
+    "revision": "92d828b4717260c0275a",
+    "url": "/VotosUni/static/js/2.09c1b0f8.chunk.js"
+  },
+  {
+    "revision": "3ab31a634c1c09baeec0c92cfbfcebab",
+    "url": "/VotosUni/static/media/cartagena.3ab31a63.jpg"
+  },
+  {
+    "revision": "b0bfa8c042d4f1b081bd6b2e243a6820",
+    "url": "/VotosUni/static/media/yerifer.b0bfa8c0.jpg"
+  },
+  {
+    "revision": "95ef317d3d8e453f2b2f3212d15d218a",
+    "url": "/VotosUni/static/media/jean.95ef317d.jpg"
+  },
+  {
+    "revision": "d2073315016253a96e1f5f19464b9f96",
+    "url": "/VotosUni/index.html"
+  }
+];
